b75a34a85db123e59e142cdc4e3bd8eb94679ee4
Patrik Sporre <patriksporre@gmail.com>
2024-10-22 22:06:11 +0200

    Lean and mean

--- boot.asm
+++ boot.asm
@@ -2,15 +2,39 @@ BITS 16                     ; Instruct NASM that this is 16 bit (real mode) code
 org 0x7c00                  ; Set the origin to 0x7c00 which is where BIOS loads the bootloader
 
 start:
-    mov si, message         ; Load the address of the 'message' string into the SI register
+    mov ax, 0xb800          ; Set AX to the video memory segment (0xb800)
+    mov es, ax              ; Load ES with the video memory segment
+    
+    xor di, di              ; Start at the beginning of the video memory (offset 0)
+    mov cx, 2000            ; 80 characters wide and 25 characters tall
+
+clearscreen:
+    mov [es:di], 0x20       ; Write character (space character) to video memory at ES:DI
+    inc di                  ; Move to next byte (the attribute)
+
+    mov [es:di], 0x07       ; Write attribute (light gray on black) to video memory at ES:DI
+    inc di                  ; Move to the next byte (the character)
+
+    dec cx                  ; Decrement CX
+    jnz clearscreen         ; Jump to 'clearscreen' if CX is not zero
+
+    lea si, [message]       ; Load the effective address of 'message' into SI
+    xor di, di              ; Start at the beginning of the video memory (offset 0)
 
 printchar:
-    lodsb                   ; Load the next byte from [SI] into AL, and increment SI
-    cmp al, 0               ; Compare the byte in AL with 0 (the null terminator)
+    mov al, [si]            ; Load the next byte from [SI] into AL
+    cmp al, 0               ; Compare the byte with 0 (the null terminator)
     je end                  ; If the byte is 0 jump to 'end'
-    mov ah, 0x0e            ; BIOS write character in TTY (teletype) mode
-    int 0x10                ; BIOS video service nterrupt
-    jmp printchar           ; Jump back to 'printchar' and print next character
+
+    mov [es:di], al         ; Write character to video memory at ES:DI
+    inc di                  ; Move to next byte (the attribute)
+
+    mov byte [es:di], 0x07  ; Attribute byte (light gray on black)
+    inc di                  ; Move to the next byte (the character)
+
+    inc si                  ; Move to the next character
+
+    jmp printchar           ; Repeat for the next character
 
 end:
     hlt                     ; Halt the CPU
